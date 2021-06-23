248d170d65f959fe2f051282b3af6a3e742b2639
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-06-23 17:01:57 -0400

    Add details page for collections
    Click on poster view allows details to pop up

---
+++ Flix/View Controllers/DetailsCollectionViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsCollectionViewController.h
+//  Flix
+//
+//  Created by mwen on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsCollectionViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
